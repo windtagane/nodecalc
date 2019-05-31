af93ae72db13b180f8f170b0b4d2e58a927321c6
Etienne M <42059747+windtagane@users.noreply.github.com>
2019-05-31 23:07:12 +0400

    Ajout Soustraction, Multiplication et Division

--- server.js
+++ server.js
@@ -9,16 +9,31 @@ app.get('/', function (req, res) {
     res.render('index.ejs');
 });
 
+// Addition
 app.post('/add', function (req, res) {
     addition = Number(req.body.number1) + Number(req.body.number2);
     res.end(addition.toString());
 });
 
-app.post('/mult', function (req, res) {
+// Soustraction
+app.post('/substract', function (req, res) {
+    multiplication = Number(req.body.number1) - Number(req.body.number2);
+    res.end(multiplication.toString());
+}); 
+
+// Multiplication
+app.post('/multiply', function (req, res) {
     multiplication = Number(req.body.number1) * Number(req.body.number2);
     res.end(multiplication.toString());
 }); 
-    
+
+// Division
+app.post('/division', function (req, res) {
+    multiplication = Number(req.body.number1) / Number(req.body.number2);
+    res.end(multiplication.toString());
+}); 
+
+// Gestion erreur 404
 app.use(function (req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page introuvable !');
