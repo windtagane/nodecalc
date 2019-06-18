a65e373b5ecc02e50947b1b75769d5fb5224b1aa
Etienne M <42059747+windtagane@users.noreply.github.com>
2019-06-18 13:31:21 +0400

    Rebuild au format calculatrice

--- server.js
+++ server.js
@@ -1,36 +1,63 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let ejs = require('ejs');
 
 let app = express();
+
+let operation = 0;
+
+app.set("view engine", "ejs"); 
 app.use(bodyParser());
 
 
 app.get('/', function (req, res) {
-    res.render('index.ejs');
+    res.render('index', { 
+        resultat : operation
+    });
 });
 
+function addition(number1, number2) {
+    let addition = number1 + number2;
+    return addition;
+}
+
+function soustraction(number1, number2) {
+    let soustraction = number1 - number2;
+    return soustraction;
+}
+
+function multiplication(number1, number2) {
+    let multiplication = number1 * number2;
+    return multiplication;
+}
+
+function division(number1, number2) {
+    let division = number1 / number2;
+    return division;
+}
+
 // Addition
-app.post('/add', function (req, res) {
-    addition = Number(req.body.number1) + Number(req.body.number2);
-    res.end(addition.toString());
+app.post('/addition', function (req, res) {
+    operation = addition(Number(req.body.number1), Number(req.body.number2));
+    res.redirect('/');
 });
 
 // Soustraction
-app.post('/substract', function (req, res) {
-    multiplication = Number(req.body.number1) - Number(req.body.number2);
-    res.end(multiplication.toString());
+app.post('/soustraction', function (req, res) {
+    operation = soustraction(Number(req.body.number1), Number(req.body.number2));
+    res.redirect('/');
 }); 
 
 // Multiplication
-app.post('/multiply', function (req, res) {
-    multiplication = Number(req.body.number1) * Number(req.body.number2);
-    res.end(multiplication.toString());
-}); 
+app.post('/multiplication', function (req, res) {
+    operation = multiplication(Number(req.body.number1), Number(req.body.number2));
+    res.redirect('/');
+});
 
 // Division
 app.post('/division', function (req, res) {
-    multiplication = Number(req.body.number1) / Number(req.body.number2);
-    res.end(multiplication.toString());
+    operation = division(Number(req.body.number1), Number(req.body.number2));
+    res.redirect('/');
 }); 
 
 // Gestion erreur 404
